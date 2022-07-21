b96246e438917124207823fa12a7991a04d0d312
chris <chrislefeefe@gmail.com>
2022-07-21 19:39:47 +0200

    fixed merges

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,17 +4,40 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {
-    public static int Task1 (ArrayList<Integer> a, ArrayList<Integer> b ){
-        for (int i = 0 ; i<a.size();i++){
-              if (i>=b.size()){
-                  return a;
+
+   public static int Task1 (List<Integer> a, List<Integer> b ){
+        int ans=0; 
+        for (int i = 0 ; i<b.size();i++){
+              if(b.get(i)>a.size() || (b.get(i)<0)){
+                i+=1;
               }
-               a+=b[i];
+              else{
+              ans+=a.get(b.get(i));
+              }
+
         }
-    }
-    return a; 
-    }
-            }
+        return ans;
+   }
+    public static List<Integer> Task4 (List<Integer> a, List<Integer> b) {
+        int sizea = a.size()-1;
+        int sizeb = a.size()-1;
+        
+        if(sizea!=sizeb)
+        {
+            return null;
+        }
+        else
+        {
+           List<Integer> c = new ArrayList<Integer>();
+                for(int i = 0; i <= sizea; i++){
+                  c.add(a.get(i)*b.get(sizeb));
+                  sizeb--;
+                }
+            return c;
+        }
+        }
+
+
     public boolean Task3(String str) {
             if(str.length() == 0)
                 return true;
@@ -33,3 +56,8 @@ public class SimpleFunctions {
         }
 
 }
+    }
+    
+  
+
+
