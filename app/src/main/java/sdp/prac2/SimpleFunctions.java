cc70afa9ddc790b754a386bac12b090b1620d279
Christophe Lefebvre <G20L2910@ict.ru.ac.za>
2022-07-21 15:12:59 +0200

    added Task1

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,15 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+    public SimpleFunctions() {
+    public static int Task1 (ArrayList<Integer> a, ArrayList<Integer> b ){
+        int ans; 
+        for (int i = 0 ; i<a.size();i++){
+           if(b.contains(a[i])){
+               ans+=a[i];
+           }
+        }
+    }
+    return ans; 
+    }
 }
