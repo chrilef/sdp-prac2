938e4fa9dfa76608bc17c5c291a4af176c5e7ab4
Anil <wimpyking@yahoo.co.za>
2022-07-21 15:15:12 +0200

    g20A2632 sol

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,18 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+    public SimpleFunctions() {
+    }
+
+    public void Task2(List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            String s = list.get(i);
+            if (s.isEmpty()) {
+                list.remove(i);
+                break;
+            }
+            list.add(i, s.substring(1));
+        }
+    }
+
 }
