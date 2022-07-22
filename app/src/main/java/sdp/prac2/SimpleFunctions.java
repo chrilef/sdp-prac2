359259627bfdb67a7d928d884c1cd2d4935543c2
chris <chrislefeefe@gmail.com>
2022-07-22 08:07:21 +0200

    updated task1 and test for task2

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {
+    }
 
    public static int Task1 (List<Integer> a, List<Integer> b ){
         int ans=0; 
@@ -13,50 +14,10 @@ public class SimpleFunctions {
               }
               else{
               ans+=a.get(b.get(i));
-              }
-
-        }
-        return ans;
-
+              }      
    }
-    public static List<Integer> Task4 (List<Integer> a, List<Integer> b) {
-        int sizea = a.size()-1;
-        int sizeb = a.size()-1;
-        
-        if(sizea!=sizeb)
-        {
-            return null;
-        }
-        else
-        {
-           List<Integer> c = new ArrayList<Integer>();
-                for(int i = 0; i <= sizea; i++){
-                  c.add(a.get(i)*b.get(sizeb));
-                  sizeb--;
-                }
-            return c;
-        }
-        }
-
-
-    public boolean Task3(String str) {
-            if(str.length() == 0)
-                return true;
-                int count = 0;
-            for(int j =0; j < str.length(); j++) {
-                if(str.charAt(j) == '(') {
-                    count += 1;
-                }
-                else if(str.charAt(j) == ')') {
-                    count -= 1;
-                    if(count < 0)
-                        return false;
-                }
-            }
-            return count == 0 ? true : false;
-        }
-
-}
+   return ans;
+   }
     }
     
   
