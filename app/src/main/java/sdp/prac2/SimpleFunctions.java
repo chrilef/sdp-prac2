e251d396b2256439b1ee1d4572cbb8683a9b18af
chris <chrislefeefe@gmail.com>
2022-07-22 07:39:51 +0200

    Messed master with last commit :\

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -32,7 +32,45 @@ public List<String> Task2(List<String> list) {
         }
         return  list;
     }
+     public static List<Integer> Task4 (List<Integer> a, List<Integer> b) {
+        int sizea = a.size()-1;
+        int sizeb = a.size()-1;
+        
+        if(sizea!=sizeb)
+        {
+            return null;
+        }
+        else
+        {
+           List<Integer> c = new ArrayList<Integer>();
+                for(int i = 0; i <= sizea; i++){
+                  c.add(a.get(i)*b.get(sizeb));
+                  sizeb--;
+                }
+            return c;
+        }
+        }
+
+
+    public boolean Task3(String str) {
+            if(str.length() == 0)
+                return true;
+                int count = 0;
+            for(int j =0; j < str.length(); j++) {
+                if(str.charAt(j) == '(') {
+                    count += 1;
+                }
+                else if(str.charAt(j) == ')') {
+                    count -= 1;
+                    if(count < 0)
+                        return false;
+                }
+            }
+            return count == 0 ? true : false;
+        }
+
 }
+
 
     
 
--- app/src/test/java/sdp/prac2/AppTest.java
+++ app/src/test/java/sdp/prac2/AppTest.java
@@ -38,5 +38,88 @@ class AppTest {
      assertIterableEquals(expected,result);
 
   }
+   @Test void task5no1() {
+        //Arrange
+        SimpleFunctions functions = new SimpleFunctions();
+        List<Integer> a= Arrays.asList(4,7,2);
+        List<Integer> expected = Arrays.asList(2,4,7);
+        //act
+        List<Integer> result = functions.Task5(a);
+        //assert
+        assertIterableEquals(expected, result);
+    }
+
+    @Test void task5no2() {
+        //Arrange
+        SimpleFunctions functions = new SimpleFunctions();
+        List<Integer> a= Arrays.asList(11,21,23);
+        List<Integer> expected = Arrays.asList(11,21,23);
+        //act
+        List<Integer> result = functions.Task5(a);
+        //assert
+        assertIterableEquals(expected, result);
+    }
+  @Test void testTask3() {
+    //Arrange
+    SimpleFunctions classBeingTested = new SimpleFunctions();
+    String str = "((())";
+    boolean expected = false;
+
+    //Act
+    boolean result = classBeingTested.Task3(str);
+    
+    //Asset
+    assertEquals(expected, result);
+  }
+
+  @Test void testTask4() {
+    //Arrange
+    SimpleFunctions classBeingTested = new SimpleFunctions();
+    List<Integer> list1 = new ArrayList<>();
+    list1.add(1);
+    list1.add(2);
+    list1.add(10);
+
+    List<Integer> list2 = new ArrayList<>();
+    list2.add(1);
+    list2.add(6);
+    list2.add(10);
+    List<Integer> expected = new ArrayList<>();
+    expected.add(10);
+    expected.add(12);
+    expected.add(10);
+    
+    //Act
+    List<Integer> result = new ArrayList<>();
+    result = classBeingTested.Task4(list1,list2);
+
+    //Asset
+    
+        assertIterableEquals(expected, result);
+  }
+  @Test void testTask4no2() {
+    //Arrange
+    SimpleFunctions classBeingTested = new SimpleFunctions();
+    List<Integer> list1 = new ArrayList<>();
+    list1.add(1);
+    list1.add(2);
+    list1.add(10);
+
+    List<Integer> list2 = new ArrayList<>();
+    list2.add(1);
+    list2.add(10);
+    List<Integer> expected = new ArrayList<>();
+    expected.add(10);
+    expected.add(12);
+    expected.add(10);
+    
+    //Act
+    List<Integer> result = new ArrayList<>();
+    result = classBeingTested.Task4(list1,list2);
+
+    //Asset
+    
+        assertIterableEquals(expected, null);
+  }
     
 }
